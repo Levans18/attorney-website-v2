662547cb66499f88892181c9eac5f1643021e22b
Liam Evans <liamevans1818@gmail.com>
2022-06-28 16:31:18 -0400

    Figured out how to use react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import  About  from "./components/About"
+import  Contact  from "./components/Contact"
+import  Home  from "./components/Home"
+import  Navigation  from "./components/NavBar"
+import  Footer  from "./components/Footer"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+
+      </Routes>
+    <Footer />
+    </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
