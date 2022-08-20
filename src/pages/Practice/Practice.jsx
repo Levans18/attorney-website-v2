6c8790191129536c92a4fe98f5158364b452c482
Liam Evans <liamevans1818@gmail.com>
2022-08-20 18:58:19 -0400

    Made a direct message part of practice

--- src/pages/Practice/Practice.jsx
+++ src/pages/Practice/Practice.jsx
@@ -8,7 +8,7 @@ import DivorceDissolution from "./PracticeAreas/DivorceDissolution";
 
 function Practice() {
   return (
-    <main id = "practice-page">
+    <main id="practice-page">
         <ul className="practice-areas">
             <li className="practice-areas">
                 <NavLink className="practice-navbar-NavLink" to={"Divorce-Dissolution"} activeclassname="active practice-navbar-NavLink">Divorce & Dissolution</NavLink>
@@ -32,93 +32,30 @@ function Practice() {
                 <NavLink className="practice-navbar-NavLink" to={"Unemployment-Appeals"} activeclassname="active practice-navbar-NavLink">Unemployment Appeals</NavLink>
             </li>
         </ul>
-        <Outlet />
-        {/* <ul className="practice-areas-shortcut">
-            <a href="#divorce-dissolution-exp">
-                <li>
-                    <h4>Divorce & Dissolution</h4>
-                </li>
-            </a>
-            <a href="#family-law-exp">
-                <li>
-                    <h4>Family Law</h4>
-                </li>
-            </a>
-            <a href="#landlord-tenant-exp">
-                <li>
-                    <h4>Landlord & Tenant</h4>
-                </li>
-            </a>
-            <a href="#discrimination-exp">
-                <li>
-                    <h4>Discrimination</h4>
-                </li>
-            </a>
-            <a href="#sexual-harrassment-exp">
-                <li>
-                    <h4>Sexual Harrassment</h4>
-                </li>
-            </a>
-            <a href="#personal-injury-exp">
-                <li>
-                    <h4>Personal Injury</h4>
-                </li>
-            </a>
-            <a href="#unemployment-appeals-exp">
-                <li>
-                    <h4>Unemployment Appeals</h4>
-                </li>
-            </a>
-        </ul>
-        <section id="divorce-dissolution-exp">
-            <h3>Divorce & Dissolution</h3>
-            <p>Todd W. Evans Lorem ipsum dolor sit amet, te nam assum nihil dissentiet. Ei vel diam saepe percipitur. Quot elit 
-                in sed, ius bonorum propriae democritum ea, te eam principes persequeris adversarium.
-                Te case putant vix, illum facilisi similique mel at. Soluta honestatis vix an, iuvaret adversarium mea id,
-                  aeterno feugait ea usu. propriae democritum ea, te eam principes persequeris ei vel diam saepe percipitur...</p>
-        </section>
-        <section id="family-law-exp">
-            <h3>Family Law</h3>
-            <p>Todd W. Evans Lorem ipsum dolor sit amet, te nam assum nihil dissentiet. Ei vel diam saepe percipitur. Quot elit 
-                in sed, ius bonorum propriae democritum ea, te eam principes persequeris adversarium.
-                Te case putant vix, illum facilisi similique mel at. Soluta honestatis vix an, iuvaret adversarium mea id,
-                  aeterno feugait ea usu. propriae democritum ea, te eam principes persequeris ei vel diam saepe percipitur...</p>
-        </section>
-        <section id="landlord-tenat-exp">
-            <h3>Landlord & Tenat</h3>
-            <p>Todd W. Evans Lorem ipsum dolor sit amet, te nam assum nihil dissentiet. Ei vel diam saepe percipitur. Quot elit 
-                in sed, ius bonorum propriae democritum ea, te eam principes persequeris adversarium.
-                Te case putant vix, illum facilisi similique mel at. Soluta honestatis vix an, iuvaret adversarium mea id,
-                eterno feugait ea usu. propriae democritum ea, te eam principes persequeris ei vel diam saepe percipitur...</p>
-        </section>
-        <section id="discrimination-exp">
-            <h3>Discrimination</h3>
-            <p>Todd W. Evans Lorem ipsum dolor sit amet, te nam assum nihil dissentiet. Ei vel diam saepe percipitur. Quot elit 
-                in sed, ius bonorum propriae democritum ea, te eam principes persequeris adversarium.
-                Te case putant vix, illum facilisi similique mel at. Soluta honestatis vix an, iuvaret adversarium mea id,
-                  aeterno feugait ea usu. propriae democritum ea, te eam principes persequeris ei vel diam saepe percipitur...</p>
-        </section>
-        <section id="sexual-harrassment-exp">
-            <h3>Sexual Harrassment</h3>
-            <p>Todd W. Evans Lorem ipsum dolor sit amet, te nam assum nihil dissentiet. Ei vel diam saepe percipitur. Quot elit 
-                in sed, ius bonorum propriae democritum ea, te eam principes persequeris adversarium.
-                Te case putant vix, illum facilisi similique mel at. Soluta honestatis vix an, iuvaret adversarium mea id,
-                  aeterno feugait ea usu. propriae democritum ea, te eam principes persequeris ei vel diam saepe percipitur...</p>
-        </section>
-        <section id="personal-injury-exp">
-            <h3>Personal Injury</h3>
-            <p>Todd W. Evans Lorem ipsum dolor sit amet, te nam assum nihil dissentiet. Ei vel diam saepe percipitur. Quot elit 
-                in sed, ius bonorum propriae democritum ea, te eam principes persequeris adversarium.
-                Te case putant vix, illum facilisi similique mel at. Soluta honestatis vix an, iuvaret adversarium mea id,
-                  aeterno feugait ea usu. propriae democritum ea, te eam principes persequeris ei vel diam saepe percipitur...</p>
-        </section>
-        <section id="unemployment-appeals-exp">
-            <h3>Unemployment Appeals</h3>
-            <p>Todd W. Evans Lorem ipsum dolor sit amet, te nam assum nihil dissentiet. Ei vel diam saepe percipitur. Quot elit 
-                in sed, ius bonorum propriae democritum ea, te eam principes persequeris adversarium.
-                Te case putant vix, illum facilisi similique mel at. Soluta honestatis vix an, iuvaret adversarium mea id,
-                  aeterno feugait ea usu. propriae democritum ea, te eam principes persequeris ei vel diam saepe percipitur...</p>
-        </section> */}
+        <div className="practice-container">
+            <section className="left">
+                <Outlet />
+            </section>
+            <section className="right">
+                <h2>Direct Email</h2>
+                <br/>
+                <form action="mailto:twevans@neo.rr.com"
+                    method="POST"
+                    enctype="multipart/form-data"
+                    name="EmailForm">
+                    Name:
+                <input type="text" size="19" name="Contact-Name"/><br/><br/>
+                    Email:
+                <input type="email" name="Contact-Email"/><br/><br/>
+                Message
+                <br/>
+                <textarea name="Contact-Message" rows="7" cols="30">
+                </textarea>
+                <br/>
+                <button type="submit" value="Submit">Send</button>
+                </form> 
+             </section>
+        </div>
   </main>
   );
 }
--- src/pages/Practice/PracticeAreas/UnemploymentAppeals.jsx
+++ src/pages/Practice/PracticeAreas/UnemploymentAppeals.jsx
@@ -3,18 +3,11 @@ import React from "react"
 function UnemploymentAppeals(){
     return(
       <>
-        <div className="practice-container">
-          <section className="left">
-            <h1>Unemployment Appeals</h1>
-            <p>Todd W. Evans Lorem ipsum dolor sit amet, te nam assum nihil dissentiet. Ei vel diam saepe percipitur. Quot elit 
-                in sed, ius bonorum propriae democritum ea, te eam principes persequeris adversarium.
-                Te case putant vix, illum facilisi similique mel at. Soluta honestatis vix an, iuvaret adversarium mea id,
-                  aeterno feugait ea usu. propriae democritum ea, te eam principes persequeris ei vel diam saepe percipitur...</p>
-          </section>
-          <section className="right">
-            <img/>
-          </section>
-        </div>
+        <h1>Unemployment Appeals</h1>
+        <p>Todd W. Evans Lorem ipsum dolor sit amet, te nam assum nihil dissentiet. Ei vel diam saepe percipitur. Quot elit 
+            in sed, ius bonorum propriae democritum ea, te eam principes persequeris adversarium.
+            Te case putant vix, illum facilisi similique mel at. Soluta honestatis vix an, iuvaret adversarium mea id,
+              aeterno feugait ea usu. propriae democritum ea, te eam principes persequeris ei vel diam saepe percipitur...</p>
       </>
     );
 }
