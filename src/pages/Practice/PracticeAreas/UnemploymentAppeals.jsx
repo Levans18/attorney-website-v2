f89c8c232fae5b8c3a20724f83f50d6ddc197842
Liam Evans <liamevans1818@gmail.com>
2022-09-12 16:49:52 -0400

    Changed Scale Logo and fixed up SH and UA

--- src/pages/Practice/PracticeAreas/UnemploymentAppeals.jsx
+++ src/pages/Practice/PracticeAreas/UnemploymentAppeals.jsx
@@ -4,10 +4,11 @@ function UnemploymentAppeals(){
     return(
       <>
         <h1>Unemployment Appeals</h1>
-        <p>Todd W. Evans Lorem ipsum dolor sit amet, te nam assum nihil dissentiet. Ei vel diam saepe percipitur. Quot elit 
-            in sed, ius bonorum propriae democritum ea, te eam principes persequeris adversarium.
-            Te case putant vix, illum facilisi similique mel at. Soluta honestatis vix an, iuvaret adversarium mea id,
-              aeterno feugait ea usu. propriae democritum ea, te eam principes persequeris ei vel diam saepe percipitur...</p>
+        <p><span className="tab"></span>Until recently, I have not had many unemployment appeals cases in the last few years.  I was very successful in getting denials reversed on appeals for clients who needed and should have
+           received unemployment benefits.  I would estimate that I was successful 75% of the time.  It is important that you reach out to an attorney once your receive what is called the Director's Redetermination. 
+            The next step in the appeal process is another appeal with a hearing.  They will try to push a telephone hearing on you but I have been more successful with asking for an in person hearing and requiring the employee to travel to Cleveland to defend the denial of benefits. 
+            However, now with the Pandemic, every hearing is by telephone.   If you lose a telephonic hearing and then contact me, my chances of helping and getting a reversal are substantially reduced to almost no chance.  So if you or anyone else believes they have been wrongly denied
+             unemployment benefits, hopefully this advise is helpful.  Feel free to pass it along.</p>
       </>
     );
 }
